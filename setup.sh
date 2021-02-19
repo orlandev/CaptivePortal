0901f7c541624c58eaa6015154d8a2d0f1bd375f
Orlando N. Rodriguez <dfashion.corp@gmail.com>
2021-02-19 09:10:32 -0500

    ADD setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+sudo apt install nginx hostapd dnsmasq net-tools python3-acme python3-certbot python3-mock python3-openssl python3-pkg-resources python3-pyparsing python3-zope.interface python3-certbot-nginx git -y
+
